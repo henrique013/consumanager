1450158d644a91d45703baac4083ccd36aef96bc
Henrique Alves <henrique013.a@gmail.com>
2016-12-28 00:12:29 -0200

    vagrant funcionandogit status

--- Vagrantfile
+++ Vagrantfile
@@ -5,9 +5,12 @@ Vagrant.configure("2") do |config|
 
   config.vm.box = "geerlingguy/ubuntu1604"
 
-  config.vm.network "private_network", ip: "192.168.56.101"
+  config.vm.hostname = File.basename(Dir.getwd) # current directory
 
-  config.vm.synced_folder ".", "/var/www/html"
+  config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 5432, host: 5432
+
+  config.vm.synced_folder ".", "/var/www/public"
 
   config.vm.provider "virtualbox" do |vb|
      vb.memory = "256"
--- provisioning.sh
+++ provisioning.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# ---------------------------------------
+#          Virtual Machine Setup
+# ---------------------------------------
+
 apt-get update
 apt-get install -y htop
 apt-get install -y curl
@@ -13,27 +17,64 @@ apt-get install -y nodejs-legacy
 apt-get install -y npm
 
 
-# PHP
-sed -i "s/short_open_tag =.*/short_open_tag = On/" /etc/php/7.0/apache2/php.ini
-sed -i "s/short_open_tag =.*/short_open_tag = On/" /etc/php/7.0/cli/php.ini
-echo "
+# ---------------------------------------
+#          PostgreSQL Setup
+# ---------------------------------------
+
+cd /etc/postgresql/9.5/main
+
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" postgresql.conf
+echo "host  all  all  10.0.2.0/24  md5" >> pg_hba.conf
+service postgresql restart
+
+sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+
+
+# ---------------------------------------
+#          PHP Setup
+# ---------------------------------------
+
+cd /etc/php/7.0
+
+xdebug="
 xdebug.remote_enable=1
 xdebug.remote_port=9000
-xdebug.remote_host=10.0.2.2" >> /etc/php/7.0/mods-available/xdebug.ini
+xdebug.remote_host=10.0.2.2"
+
+echo "${xdebug}" >> mods-available/xdebug.ini
+sed -i "s/short_open_tag =.*/short_open_tag = On/" apache2/php.ini
+sed -i "s/short_open_tag =.*/short_open_tag = On/" cli/php.ini
 
 
-# apache
-a2enmod headers
+# ---------------------------------------
+#          Apache Setup
+# ---------------------------------------
+
+vhost="
+<VirtualHost *:80>
+  DocumentRoot /var/www/public
+  ServerName localhost
+  <Directory /var/www/public>
+    AllowOverride All
+  </Directory>
+</VirtualHost>"
+
+echo "${vhost}" > /etc/apache2/sites-available/site_vagrant.conf
+a2ensite site_vagrant.conf
+
+a2enmod rewrite
 service apache2 restart
 
 
-# dependencias - globais
+# ---------------------------------------
+#       Tools Setup.
+# ---------------------------------------
+
+cd /var/www/public
+
 npm install gulp-cli@1.2.2 --global
 npm install bower@1.8.0 --global
 
-
-# dependencias
-cd /var/www/html
 npm install
 bower install
 composer install
